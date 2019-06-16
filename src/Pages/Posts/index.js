8facabdb5271efc0963382cb2722f450dd37e833
Daniar <daniar.ah55@gmail.com>
2019-06-16 13:19:15 +0600

    Initial commit

---
+++ src/Pages/Posts/index.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+export default class Posts extends Component {
+
+    state = {
+        posts: [],
+        name: 'asd'
+    };
+
+    async componentWillMount(){
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let json = await response.json();
+        this.setState({
+            posts: json
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                {
+                    this.state.posts.map((post) => (
+                        <a href={`/post/${post.id}`}><li>{post.title}</li></a>
+                    ))
+                }
+            </ul>
+        )
+    }
+}
+
